58aedfe8bba70d5fd24d8cb74b95e349ad5e0a48
Jonatan Sanchez <jonatansanchez0199@gmail.com>
2026-04-09 14:14:19 -0400

    fix rate limiter order

--- auth-2fa-project/server/index.js
+++ auth-2fa-project/server/index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const app = express();
+
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use('api/login', loginLimiter);
-
 // ruta de prueba
 app.get('/', (req, res) => {
   res.send('API funcionando 🚀');
@@ -27,10 +27,11 @@ app.use('/api', authRoutes);
 console.log(process.env.DATABASE_URL);
 
 //limitador de login
-const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
   message: 'Demasiados intentos de login. Intenta más tarde.'
 });
+
+app.use('/api/login', loginLimiter);
