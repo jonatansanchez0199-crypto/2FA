b39eb32ff581537534d1d2ce75e0281c05bea277
Jonatan Sanchez <jonatansanchez0199@gmail.com>
2026-04-23 14:41:58 -0400

    Reorganizacion de Rutas authRoutes.js

--- auth-2fa-project/server/routes/authRoutes.js
+++ auth-2fa-project/server/routes/authRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, generate2FA } = require('../controllers/authController');
+const {
+  register,
+  login,
+  generate2FA,
+  verify2FA,
+  login2FA,
+  changePassword,
+  deleteUser,
+  disable2FA
+} = require('../controllers/authController');
+
 const authMiddleware = require('../middleware/authMiddleware');
 
+// rutas
 router.post('/register', register);
 router.post('/login', login);
+router.post('/login/2fa', login2FA);
 
 router.get('/profile', authMiddleware, (req, res) => {
   res.json({
@@ -15,23 +27,10 @@ router.get('/profile', authMiddleware, (req, res) => {
 });
 
 router.post('/2fa/generate', authMiddleware, generate2FA);
-
-module.exports = router;
-
-const { verify2FA } = require('../controllers/authController');
-
 router.post('/2fa/verify', authMiddleware, verify2FA);
-
-const { login2FA } = require('../controllers/authController');
-
-router.post('/login/2fa', login2FA);
-
-const { changePassword } = require('../controllers/authController');
+router.post('/2fa/disable', authMiddleware, disable2FA);
 
 router.put('/users/password', authMiddleware, changePassword);
-
-const { deleteUser } = require('../controllers/authController');
 router.delete('/users/delete', authMiddleware, deleteUser);
 
-const { disable2FA } = require('../controllers/authController');
-router.post('/2fa/disable', authMiddleware, disable2FA);
+module.exports = router;
